f18b22f15507b84049183dbc61ae5aeffb7aca40
Kara Kincaid <KKincaid@empathylab.com>
2014-09-05 16:08:45 -0400

    Tweak for loop so it shows the correct data, add minor styling

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,17 @@ $(document).ready(function(){
 
 		var newLog = [];
 
-		$('body').append("<h1>" + data.date + "</h1>");
+		$(".date").append(data.date);
 
 		for(var i = 0; i < data.exercises.length; i++) {
-			$('body').append("<h2>" + data.exercises[i].exerciseName + "<span>" + data.exercises[i].notes + "</span></h2>");
+			$(".exercise").append("<h3>" + data.exercises[i].exerciseName + "<span>" + data.exercises[i].notes + "</span></h3>");
 			for(var x = 0; x < data.exercises[i].details.length; x++) {
-				$('body').append("<ul><li>" + data.exercises[i].details[i].sets + "x" + data.exercises[i].details[i].reps + " @ " + data.exercises[i].details[i].weight + data.exercises[i].details[i].measurement + "</li></ul>");
+				$(".exercise").append("<ul><li>" + data.exercises[i].details[x].sets + "x" + data.exercises[i].details[i].reps + " @ " + data.exercises[i].details[x].weight + data.exercises[i].details[x].measurement + "</li></ul>");
 
 				newLog.push(data.exercises[i].details[x]);
-			
 			}
-			
 		}
-		
-		});
+	});
+
+	
 });
