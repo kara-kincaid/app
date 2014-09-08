0bd07911b64f9e3935b745b0ccd1992a9d7f1f35
Kara Kincaid <KKincaid@empathylab.com>
2014-09-08 16:14:32 -0400

    Add minor styling, update add page with correct inputs and include js for adding additional sets/reps

--- js/scripts.js
+++ js/scripts.js
@@ -19,5 +19,28 @@ $(document).ready(function(){
 		}
 	});
 
-	
+	$('<div/>', {
+         'class' : 'additional-set', html: GetHtml()
+     }).appendTo('.additional-sets-here');
+     $('.add-set').click(function () {
+           $('<div/>', {
+               'class' : 'extraPerson', html: GetHtml()
+     }).hide().appendTo('.additional-sets-here').slideDown('slow');
+         
+     });
+
+
+    function GetHtml() {
+		var len = $('.additional-set').length;
+		var $html = $('.additional-set-template').clone();
+		$html.find('[name=sets]')[0].name="sets" + len;
+		$html.find('[name=reps]')[0].name="reps" + len;
+		$html.find('[name=weight]')[0].name="weight" + len;
+		$html.find('[name=measurement]')[0].name="measurement" + len;
+		return $html.html();
+	}
+	// when .add-set is clicked, need to add (repeat?) form #set-0 and update form id to be #set-1, #set-2, etc...
+
+	// when .add-exercise is clicked, need to add all new form (or new page?)
+
 });
